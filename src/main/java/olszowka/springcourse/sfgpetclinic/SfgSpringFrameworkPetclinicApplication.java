fd9e9d67bdda55b85df89fb1f08fb69b2b17390f
ToJoos <joosx2000@gmail.com>
2021-07-29 17:50:06 +0200

    Initial commit.

---
+++ src/main/java/olszowka/springcourse/sfgpetclinic/SfgSpringFrameworkPetclinicApplication.java
@@ -0,0 +1,13 @@
+package olszowka.springcourse.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringFrameworkPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpringFrameworkPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/olszowka/springcourse/sfgpetclinic/SfgSpringFrameworkPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package olszowka.springcourse.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringFrameworkPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
