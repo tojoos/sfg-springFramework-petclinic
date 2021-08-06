cabcf11d4b3de03487a3b01078e271e376e0f05f
ToJoos <joosx2000@gmail.com>
2021-08-06 13:52:23 +0200

    Map baes service implementation - should fix CrudService implementation. Closes #6.

---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package olszowka.springcourse.sfgpetclinic.services.map;
+
+import olszowka.springcourse.sfgpetclinic.model.Owner;
+import olszowka.springcourse.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner save(Long id, Owner object) {
+        return super.save(id, object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package olszowka.springcourse.sfgpetclinic.services.map;
+
+import olszowka.springcourse.sfgpetclinic.model.Vet;
+import olszowka.springcourse.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
