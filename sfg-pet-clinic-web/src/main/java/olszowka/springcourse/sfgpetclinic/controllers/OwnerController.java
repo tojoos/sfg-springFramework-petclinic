b8ed745ef5ad2c80416ff09a33b0d935c2b4d812
ToJoos <joosx2000@gmail.com>
2021-08-10 08:51:59 +0200

    Creating owner page and controller. Closes #13.

--- sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,12 @@ package olszowka.springcourse.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-public class indexController {
+public class OwnerController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
-        return "index";
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
     }
 }
