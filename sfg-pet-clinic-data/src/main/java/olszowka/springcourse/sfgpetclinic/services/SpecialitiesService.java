bc67b5289a9e979ae8f28a9830974ecd422ab3f9
ToJoos <joosx2000@gmail.com>
2021-08-17 12:17:17 +0200

    125. Added Specialities Map Service Impl. Closes #31.

---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package olszowka.springcourse.sfgpetclinic.services;
+
+import olszowka.springcourse.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality> {
+
+}
