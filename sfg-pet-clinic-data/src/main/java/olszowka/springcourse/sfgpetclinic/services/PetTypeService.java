3f81d7ee6ae3631fceabb9d90d6b60526f95951f
ToJoos <joosx2000@gmail.com>
2021-08-17 11:31:04 +0200

    122. Added PetTypeMapServiceImpl. Closes #29.

---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package olszowka.springcourse.sfgpetclinic.services;
+
+import olszowka.springcourse.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType> {
+}
---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package olszowka.springcourse.sfgpetclinic.services.map;
+
+import olszowka.springcourse.sfgpetclinic.model.PetType;
+import olszowka.springcourse.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
