9214f3cffd48ee33fc8db60b54f3e90871b95241
ToJoos <joosx2000@gmail.com>
2021-08-25 14:53:01 +0200

    148. Convert Vets to JPA entities. Closes #37.

--- sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/model/Pet.java
@@ -10,16 +10,17 @@ public class Pet extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "type_id")
-    private PetType petType;
-
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
     @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
 
     public String getName() {
         return name;
