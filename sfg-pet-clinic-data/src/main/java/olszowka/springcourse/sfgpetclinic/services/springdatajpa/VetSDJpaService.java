bc81c0b52b1444777d16a3775d6c597c01989dc9
ToJoos <joosx2000@gmail.com>
2021-08-26 14:24:28 +0200

    Implementing JPA service for Vets. Closes #41.

---
+++ sfg-pet-clinic-data/src/main/java/olszowka/springcourse/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package olszowka.springcourse.sfgpetclinic.services.springdatajpa;
+
+import olszowka.springcourse.sfgpetclinic.model.Vet;
+import olszowka.springcourse.sfgpetclinic.repositories.VetRepository;
+import olszowka.springcourse.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
