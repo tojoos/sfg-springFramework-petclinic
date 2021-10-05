d0c0c97d364cc4e57f2153b723a260927ed39ea6
ToJoos <joosx2000@gmail.com>
2021-10-05 12:18:59 +0200

    Added WebDataBinder to protect "id" fields from manipulation in an application.

--- sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package olszowka.springcourse.sfgpetclinic.controllers;
 import olszowka.springcourse.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +17,11 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedField(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @GetMapping({"", "/", "/index", "/index.html"})
