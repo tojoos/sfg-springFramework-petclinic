3b2ce553a4b695f7cf8ba00438fe271033b82201
ToJoos <joosx2000@gmail.com>
2021-10-05 11:58:16 +0200

    Added page displaying each owner.

--- sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic-web/src/main/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerController.java
@@ -3,26 +3,35 @@ package olszowka.springcourse.sfgpetclinic.controllers;
 import olszowka.springcourse.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
     private final OwnerService ownerService;
 
-    //@Autowired <- automatically by Spring Framework
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String findOwner() {
         return "notImplemented";
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showOwner(@PathVariable String id) {
+        ModelAndView mav = new ModelAndView("owners/show");
+        mav.addObject("owner", ownerService.findById(Long.valueOf(id)));
+        return mav;
     }
 }
--- sfg-pet-clinic-web/src/test/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerControllerTest.java
+++ sfg-pet-clinic-web/src/test/java/olszowka/springcourse/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -7,18 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -74,5 +70,18 @@ class OwnerControllerTest {
                 .andExpect(view().name("notImplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void showOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))))
+                .andExpect(view().name("owners/show"));
+
+        verify(ownerService, times(1)).findById(anyLong());
     }
 }
